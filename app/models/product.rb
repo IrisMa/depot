e17b36b36dc5f3eef5bf2ee1a6d323edcc1a1355
IrisMa <mistmilo@gmail.com>
2013-07-15 23:20:31 +0800

    Show catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
-  #Validation
+  default_scope :order => 'title'
+
+  #Validation stuff
   validates :title, :description, :image_url, :presence =>true #to validate if there's content
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01} #to validate if the price has a valid number
   validates :title, :uniqueness => true #to validate if the title is unique
