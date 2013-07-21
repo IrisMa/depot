212a43ddb0ee8b12c2aedc3ba7bd94624009d371
IrisMa <mistmilo@gmail.com>
2013-07-21 20:55:33 +0800

    validate product title, minimum length is 10

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,10 +10,12 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, :presence =>true #to validate if there's content
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01} #to validate if the price has a valid number
   validates :title, :uniqueness => true #to validate if the title is unique
+  validates :title, :length => { minimum:10 }
   validates :image_url, :format => {
       :with =>%r{\.(gir|jpg|png)$}i,
       :message => 'must be a URL for GIF,JPG or PNG image.'
   }
+
 
   private
 
