4414a81a0444c1703c8b0a42aac25f46722d5f1f
IrisMa <mistmilo@gmail.com>
2013-08-07 21:45:21 +0800

    Email Notifications

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -7,9 +7,9 @@ class Notifier < ActionMailer::Base
   #   en.notifier.order_received.subject
   #
   def order_received
-    @greeting = "Hi"
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, :subject => 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,8 +18,8 @@ class Notifier < ActionMailer::Base
   #   en.notifier.order_shipped.subject
   #
   def order_shipped
-    @greeting = "Hi"
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, :subject => 'Pragmatic Store Order Shipped'
   end
 end
