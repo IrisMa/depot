4213563183a060bd42dca80243e76f233cc2ccc0
IrisMa <mistmilo@gmail.com>
2013-08-26 23:28:54 +0800

    Limmiting access

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_filter :authorize
   def new
   end
 
   def create
-    if user == User.authenticate(params[:name], params[:password])
+    if user = User.authenticate(params[:name], params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
